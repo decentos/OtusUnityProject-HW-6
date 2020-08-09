88c7a80001beaa923b7ce9a4c06e6a51af39de95
Vlad Matveev <matveev_vlad@me.com>
2020-08-09 17:53:52 +0300

    Moving platform between more points

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,32 +4,23 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Transform top;
-    public Transform bottom;
     public float speed;
+    public Vector3[] currentPoint = new Vector3[3];
+    int targetIndex;
 
-    private float current; // от 0 до 1
-    private float dir;
-
-    // Start is called before the first frame update
     void Start()
     {
-        current = 0.0f;
-        dir = 1.0f;
+        targetIndex = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        current += dir * speed * Time.deltaTime;
-        if (current > 1.0f) {
-            current = 1.0f;
-            dir = -1.0f;
-        } else if (current < 0.0f) {
-            current = 0.0f;
-            dir = 1.0f;
-        }
 
-        transform.position = Vector3.Lerp(top.position, bottom.position, current);
+        transform.position = Vector3.MoveTowards(transform.position, currentPoint[targetIndex], Time.deltaTime * speed);
+        
+        if (transform.position == currentPoint[targetIndex])
+        {
+            targetIndex = (targetIndex + 1) % currentPoint.Length;
+        }
     }
 }

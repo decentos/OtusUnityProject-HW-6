6a98120d1b4d038dd9471078a81b2187dbbb99fd
Vlad Matveev <matveev_vlad@me.com>
2020-08-09 16:56:35 +0300

    Create restart menu after lose

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    bool isLose;
+    public CanvasGroup logicButtonsCanvasGroup;
+    public CanvasGroup playerLoseCanvasGroup;
+    public Character character;
+
+    void Start()
+    {
+        isLose = false;
+        Utility.SetCanvasGroupEnabled(playerLoseCanvasGroup, false);
+    }
+
+    void Update()
+    {
+        if (character.transform.position.y < -5.5f) {
+            PlayerLost();
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void PlayerLost()
+    {
+        Debug.Log("Player lost");
+        Utility.SetCanvasGroupEnabled(logicButtonsCanvasGroup, false);
+        Utility.SetCanvasGroupEnabled(playerLoseCanvasGroup, true);
+    }
+}
